fadec3a33b6af1b7bd8570bcccffa0bea7aa7209
usmaarn <usmaarn@gmail.com>
2023-10-10 06:09:24 +0100

    created the donatify application

---
+++ database/migrations/2023_10_10_050827_create_donations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('donations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->noActionOnDelete();
+            $table->string('title');
+            $table->string('description');
+            $table->longText('full_description')->nullable();
+            $table->string('thumbnail');
+            $table->string('target');
+            $table->string('type');
+            $table->mediumInteger('realised');
+            $table->string('deadline');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('donations');
+    }
+};
