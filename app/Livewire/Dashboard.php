e126d95a25de4878bd00d7a888d7508d2a9160bc
usmaarn <usmaarn@gmail.com>
2023-10-11 16:05:42 +0100

    updated the dashboard

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Donation;
 use Livewire\Component;
 
 class Dashboard extends Component
@@ -12,6 +13,22 @@ class Dashboard extends Component
     {
         $this->donations = auth()->user()->donations;
     }
+
+    public function filterCompleted()
+    {
+        $this->donations =  auth()->user()->donations()->where('completed', true)->latest()->get();
+    }
+
+    public function filterUncompleted()
+    {
+        $this->donations = auth()->user()->donations()->where('completed', false)->latest()->get();
+    }
+
+    public function filterAll()
+    {
+        $this->donations =  auth()->user()->donations()->latest()->get();
+    }
+
     public function render()
     {
         return view('dashboard');
